220977002663f3bb5023b16a6770bbc8d662f7f5
Gustavo <gustavoluigib@gmail.com>
2020-11-12 13:55:50 -0300

    chore innocence

---
+++ src/pages/innocence/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../../assets/img/Home.jpg';
+
+import { Container } from './styles';
+
+export default function Innocence() {
+  return (
+    <Container>
+      <img src={Home} alt="" />
+    </Container>
+  );
+}
---
+++ src/pages/innocence/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  img {
+    width: 100%;
+    max-width: 1440px;
+    margin: auto;
+  }
+`;
